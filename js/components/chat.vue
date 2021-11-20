42477671f218c9b4c6909eca922f902d023844c5
jekerr <jekerr96@gmail.com>
2021-11-20 14:00:31 +0700

    + отправка картинок

--- js/components/chat.vue
+++ js/components/chat.vue
@@ -18,13 +18,13 @@
                     <img src="/images/smile.svg" alt="">
                     <transition name="opacity">
                         <div class="smiles custom-scrollbar" v-show="showSmiles">
-                            <img loading="lazy" v-for="item in 189" :src="'/images/smile/Emoji Smiley-' + (item.toString().padStart(2, '0')) + '.png'" alt="">
+                            <img @click="onSmileClick($event)" loading="lazy" v-for="item in 189" :src="'/images/smile/Emoji Smiley-' + (item.toString().padStart(2, '0')) + '.png'" alt="">
                         </div>
                     </transition>
                 </button>
                 <button class="chat__tools-item" @click="$refs.fileLoad.click()">
                     <img src="/images/load-image.svg" alt="">
-                    <input ref="fileLoad" type="file" multiple="multiple" hidden>
+                    <input ref="fileLoad" type="file" multiple="multiple" hidden @change="loadFiles($event)">
                 </button>
                 <button class="chat__tools-item">
                     <img src="/images/mic.svg" alt="">
@@ -49,6 +49,11 @@ export default {
             messageSound: new Audio('../sound/new_message.wav'),
         };
     },
+    computed: {
+        name: function () {
+            return this.myName ? this.myName : 'Аноним';
+        }
+    },
     methods: {
         onInput: function(ev) {
             this.message = ev.target.innerHTML;
@@ -70,7 +75,7 @@ export default {
 
             this.socket.emit('chatMsg', {
                 id: this.myId,
-                name: this.myName ? this.myName : 'Анонним',
+                name: this.name,
                 message: this.message,
                 roomName: this.roomName,
                 timestamp: Date.now(),
@@ -78,6 +83,61 @@ export default {
 
             ev.target.innerHTML = '';
             this.message = '';
+        },
+
+        onSmileClick(ev) {
+            const img = new Image();
+            img.src = ev.target.src;
+            img.className = 'chat-smile';
+            this.$refs.writeMessage.focus();
+
+            const txt = window.getSelection().getRangeAt(0);
+            txt.insertNode(img);
+            this.setCursorAfterElement(img);
+            this.message = this.$refs.writeMessage.innerHTML;
+        },
+
+        setCursorAfterElement(element) {
+            let selection = window.getSelection();
+            let range = document.createRange();
+            range.setStartAfter(element);
+            selection.removeAllRanges();
+            selection.addRange(range);
+        },
+
+        loadFiles(ev) {
+            this.encodeImagesAsURL(ev.target.files, images => {
+                this.socket.emit('chatMsg', {
+                    name: this.name,
+                    message: images,
+                    type: 'img',
+                    roomName: this.roomName,
+                    timestamp: Date.now(),
+                });
+            });
+        },
+
+        encodeImagesAsURL(files, cb) {
+            const images = Array.from(files).filter(file => file.type.match('image.*'));
+            const encodedImages = [];
+
+            for (const file of files) {
+                const reader = new FileReader();
+
+                reader.onload = ev => {
+                    encodedImages.push(ev.target.result);
+
+                    if (encodedImages.length === images.length) {
+                        cb(encodedImages);
+                    }
+                }
+
+                reader.onerror = ev => {
+                    console.error(ev);
+                }
+
+                reader.readAsDataURL(file);
+            }
         }
     },
     created() {
@@ -86,6 +146,8 @@ export default {
         this.socket.on('chatMsg', data => {
             const message = data;
             message.my = data.id === this.myId;
+
+            console.log(data);
 
             switch (message.type) {
                 case 'exit':
--- js/components/message.vue
+++ js/components/message.vue
@@ -1,7 +1,10 @@
 <template>
     <div class="message" :class="{'message--my': message.my}">
         <div class="message__name" v-if="message.name">{{ message.name }}:</div>
-        <div class="message__content" v-html="message.message"></div>
+        <div class="message__content" v-if="!message.type || message.type === 'exit'" v-html="message.message"></div>
+        <div class="message__content" v-if="message.type === 'img'">
+            <img v-for="imgSrc in message.message" :src="imgSrc" alt="">
+        </div>
     </div>
 </template>
 
