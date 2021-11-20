0a593c52cc6495039036db7fae5dc9bfd6199a87
jekerr <jekerr96@gmail.com>
2021-11-20 17:48:55 +0700

    * log

--- js/components/message.vue
+++ js/components/message.vue
@@ -20,6 +20,7 @@ export default {
     props: ['message'],
     methods: {
         convertAudioToSrc(audio) {
+            console.log(audio);
             return window.URL.createObjectURL(audio)
         }
     }
